453c39d5df38c90e2fdb95b0493a86b266b91254
William Rivera <willitorivera@gmail.com>
2017-08-18 17:26:03 -0400

    Add project files.

---
+++ MongoMigrate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoMigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            
+        }
+    }
+}
